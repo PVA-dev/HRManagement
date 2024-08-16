8e4ee95cf1edf375f8802d88ad781e235d64c618
pva-dev <podokvadim@gmail.com>
2024-08-16 15:39:24 +0500

    Добавил контекс БД и SeedData

---
+++ HRManagement/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using HRManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRManagement.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Bonus> Bonuses { get; set; }
+        public DbSet<BonusType> BonusTypes { get; set; }
+        public DbSet<Candidate> Candidates { get; set; }
+        public DbSet<CandidateStatus> CandidateStatuses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<PersonalInfo> PersonalInfos { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Vacancy> Vacancies { get; set; }
+        public DbSet<VacancyStatus> VacancyStatuses { get; set; }   
+    }
+}
--- HRManagement/Program.cs
+++ HRManagement/Program.cs
@@ -1,13 +1,25 @@
+using HRManagement.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (args.Length == 1 && args[0] == "seeddata")
+{
+	Seed.SeedData(app);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
