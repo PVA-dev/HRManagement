911c489c14192a25caaf87b00e5c1e4d2aa88093
pva-dev <podokvadim@gmail.com>
2024-08-16 15:42:53 +0500

    Добавил generic репозиторий

---
+++ HRManagement/Data/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using HRManagement.Models;
+
+namespace HRManagement.Data.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<bool> AddAsync(T entity);
+        Task<bool> DeleteAsync(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<bool> SaveAsync();
+        Task<bool> UpdateAsync(T entity);
+    }
+}
---
+++ HRManagement/Data/Repositories/Repository.cs
@@ -0,0 +1,49 @@
+﻿using HRManagement.Data.Interfaces;
+using HRManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRManagement.Data.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly AppDbContext _dbContext;
+
+        public Repository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<bool> AddAsync(T entity)
+        {
+            _dbContext.Add(entity);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> DeleteAsync(T entity)
+        {
+            _dbContext.Remove(entity);
+            return await SaveAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            var saved = await _dbContext.SaveChangesAsync();
+            return saved > 0;
+        }
+
+        public async Task<bool> UpdateAsync(T entity)
+        {
+            _dbContext.Update(entity);
+            return await SaveAsync();
+        }
+    }
+}
